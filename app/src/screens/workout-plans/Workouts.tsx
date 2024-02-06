76971143d385bbf2c0e6f7ed87c7caaa0ebff5c5
Toni <smokercho56@gmail.com>
2024-02-06 22:31:21 +0200

    Implemented pull up to refresh functionality using the hook useRefreshControl

--- app/src/screens/workout-plans/Workouts.tsx
+++ app/src/screens/workout-plans/Workouts.tsx
@@ -1,10 +1,12 @@
-import { FlatList } from "react-native";
+import { FlatList, RefreshControl } from "react-native";
 import Screen from "../../components/common/Screen";
 
 import WorkoutPlanCard from "../../components/workouts/WorkoutPlanCard";
 import useFetchWorkoutPlans from "../../hooks/useFetchWorkoutPlans";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { WorkoutsStackParamList } from "../../Stacks/WorkoutsStack";
+import { useCallback, useEffect, useState } from "react";
+import useRefreshControl from "../../hooks/useRefreshControl";
 
 interface WorkoutsProps {
   navigation: StackNavigationProp<WorkoutsStackParamList>;
@@ -12,8 +14,10 @@ interface WorkoutsProps {
 
 const Workouts: React.FC<WorkoutsProps> = ({ navigation }) => {
   // TODO: Add data type
-  const { data } = useFetchWorkoutPlans();
+  const { data, refetch, isLoading } = useFetchWorkoutPlans();
   // TODO: Add a skeleton
+
+  const {refreshing, onRefresh} = useRefreshControl({refreshFn: refetch, isLoading: isLoading})
 
   return (
     <Screen>
@@ -22,6 +26,9 @@ const Workouts: React.FC<WorkoutsProps> = ({ navigation }) => {
       <FlatList
         style={{ paddingTop: 15, paddingLeft: 5, paddingRight: 5, flex: 1 }}
         data={data}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
         renderItem={({ item, index }) => (
           <WorkoutPlanCard key={index} navigation={navigation} plan={item} />
         )}
