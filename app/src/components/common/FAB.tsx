34ab574cc6bb4d840b8ffc64ed8c69ea71750be4
Toni <smokercho56@gmail.com>
2024-03-20 21:54:14 +0200

    Created Floating action button component FAB

---
+++ app/src/components/common/FAB.tsx
@@ -0,0 +1,40 @@
+import { ReactNode } from "react";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
+import { useTheme } from "../../contexts/ThemeContext";
+
+interface FABProps {
+  onPress: () => void;
+  testId?: string;
+  icon?: ReactNode;
+  type?: "primary" | "outlined" | "text";
+  loading?: boolean;
+}
+
+const FAB: React.FC<FABProps> = ({ testId, icon, onPress, loading }) => {
+  const { colors } = useTheme();
+
+  const styles = StyleSheet.create({
+    wrapper: {
+      backgroundColor: colors.button,
+      minWidth: 45,
+      minHeight: 45,
+      borderRadius: 100,
+      justifyContent: "center",
+      alignItems: "center",
+      flexDirection: "row",
+    },
+
+    iconWrapper: {
+      justifyContent: "center",
+      alignItems: "center",
+    },
+  });
+
+  return (
+    <TouchableOpacity style={styles.wrapper} testID={testId} onPress={onPress}>
+      <View style={styles.iconWrapper}>{icon}</View>
+    </TouchableOpacity>
+  );
+};
+
+export default FAB;
